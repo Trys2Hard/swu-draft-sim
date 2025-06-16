f87ad23c12c540f48987d595e150545374a9e439
Keith LePoidevin <klepoidevin3@gmail.com>
2025-06-15 18:07:05 -0700

    Complete Pack Creation and Card Pick Functionality
    Refactor Pack, fix bugs, and complete functionality for creating packs and selecting draft cards.

--- client/src/Pack.jsx
+++ client/src/Pack.jsx
@@ -10,40 +10,120 @@ export default function Pack() {
     const [rareNum, setRareNum] = useState(1);
     const [uncommonNum, setUncommonNum] = useState(3);
     const [commonNum, setCommonNum] = useState(10);
+    const [pickNum, setPickNum] = useState(1);
+    const [packNum, setPackNum] = useState(1);
+    const [title, setTitle] = useState('Leaders');
+    const [isFetching, setIsFetching] = useState(false);
 
     let didRun = useRef(false);
+    let didRunCreateCards = useRef(false);
 
     useEffect(() => {
         if (didRun.current) return;
         didRun.current = true;
-        async function fetchCard() {
-            const res = await fetch('http://localhost:3000/leader');
-            const card = await res.json();
-            setPack((prevPack) => [...prevPack, card]);
-        }
+        const createLeaderPack = async () => {
+            async function fetchLeaders() {
+                const res = await fetch('http://localhost:3000/leader');
+                const card = await res.json();
+                setPack((prevPack) => [...prevPack, card]);
+            }
 
-        for (let i = 0; i < leaderNum; i++) {
-            fetchCard();
+            for (let i = 0; i < leaderNum; i++) {
+                fetchLeaders();
+            }
         }
-    })
+        createLeaderPack();
+    }, [leaderNum])
+
+    useEffect(() => {
+        if (leaderNum === 0) {
+            setPickNum(1);
+            setTitle('Cards');
+        }
+    }, [leaderNum]);
+
+
+    useEffect(() => {
+        if (didRunCreateCards.current || leaderNum !== 0) return;
+        didRunCreateCards.current = true;
+
+        const createCardPack = async () => {
+            if (isFetching) return;
+            setIsFetching(true);
+
+
+            if (packNum === 4) {
+                setTitle('Draft Complete');
+                setPackNum(null);
+                setPickNum(null);
+            }
+
+            async function fetchRareCard() {
+                const res = await fetch('http://localhost:3000/rare');
+                const card = await res.json();
+                setPack((prevPack) => [...prevPack, card]);
+            }
+
+            async function fetchUncommonCards() {
+                const res = await fetch('http://localhost:3000/uncommon');
+                const card = await res.json();
+                setPack((prevPack) => [...prevPack, card]);
+            }
+
+            async function fetchCommonCards() {
+                const res = await fetch('http://localhost:3000/common');
+                const card = await res.json();
+                setPack((prevPack) => [...prevPack, card]);
+            }
+
+            if (rareNum === 0 && uncommonNum === 0 && commonNum === 0) {
+                setPackNum((prev) => prev + 1);
+            }
+
+            if (pickNum === 1) {
+                setRareNum(1);
+                setUncommonNum(3);
+                setCommonNum(10);
+            }
+
+            if ((leaderNum === 0) && (packNum < 4 && packNum !== null)) {
+                for (let i = 0; i < rareNum; i++) {
+                    await fetchRareCard();
+                }
+
+                for (let i = 0; i < uncommonNum; i++) {
+                    await fetchUncommonCards();
+                }
+
+                for (let i = 0; i < commonNum; i++) {
+                    await fetchCommonCards();
+                }
+            }
+            didRunCreateCards.current = false;
+            setIsFetching(false);
+        };
+
+        createCardPack();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [leaderNum, packNum, rareNum, uncommonNum, commonNum, setTitle, setPackNum, setPickNum, setPack, pickNum])
 
     const pickCard = (id) => {
+        if (isFetching || pack.length === 0) return;
+
         const pickedCard = pack.find((card) => card.cardData?._id === id);
         if (!pickedCard) return;
 
         const pickedCardData = pickedCard.cardData;
 
-        if (deckLeaders.length < 3) {
-            setDeckLeaders((prev) => [...prev, pickedCardData]);
-        } else {
-            setDeckCards((prev) => [...prev, pickedCardData]);
-        }
+        const addCard = deckLeaders.length < 3 ? setDeckLeaders : setDeckCards;
+        addCard((prev) => [...prev, pickedCardData]);
 
         setPack([]);
 
         if (leaderNum > 0) {
             didRun.current = false;
             setLeaderNum((prevLeaderNum) => prevLeaderNum - 1);
+            setPickNum((prev) => prev + 1);
             return;
         }
 
@@ -63,45 +143,9 @@ export default function Pack() {
         } else if (rarity === 'common') {
             setCommonNum((prev) => prev - 1);
         }
+
+        setPickNum(prev => (prev >= 14 ? 1 : prev + 1));
     };
-
-    useEffect(() => {
-        const fetchCards = async () => {
-            async function fetchRareCard() {
-                const res = await fetch('http://localhost:3000/rare');
-                const card = await res.json();
-                setPack((prevPack) => [...prevPack, card]);
-            }
-
-            async function fetchUncommonCards() {
-                const res = await fetch('http://localhost:3000/uncommon');
-                const card = await res.json();
-                setPack((prevPack) => [...prevPack, card]);
-            }
-
-            async function fetchCommonCards() {
-                const res = await fetch('http://localhost:3000/common');
-                const card = await res.json();
-                setPack((prevPack) => [...prevPack, card]);
-            }
-
-            if (leaderNum === 0) {
-                for (let i = 0; i < rareNum; i++) {
-                    await fetchRareCard();
-                }
-
-                for (let i = 0; i < uncommonNum; i++) {
-                    await fetchUncommonCards();
-                }
-
-                for (let i = 0; i < commonNum; i++) {
-                    await fetchCommonCards();
-                }
-            }
-        };
-
-        fetchCards();
-    }, [leaderNum, rareNum, uncommonNum, commonNum]);
 
     const styles = {
         packBox: {
@@ -140,7 +184,9 @@ export default function Pack() {
         <>
             <Typography variant='h2' component='h1' sx={{ textAlign: 'center', mt: '2rem', color: 'white' }} >Star Wars Unlimited Draft Simulator</Typography>
             <Box sx={styles.packBox}>
-                <Typography variant='h3' component='h2' sx={{ mb: '1rem' }}>Pack 1</Typography>
+                <Typography variant='h2' component='h2' sx={{ mb: '1rem' }}>{title}</Typography>
+                <Typography variant='h3' component='h3' sx={{ mb: '1rem' }}>Pack: {packNum}</Typography>
+                <Typography variant='h3' component='h3' sx={{ mb: '1rem' }}>Pick: {pickNum}</Typography>
                 <List sx={styles.pack}>
                     {pack.map((card) => {
                         const labelId = `card-id-${card.cardData._id}`;
