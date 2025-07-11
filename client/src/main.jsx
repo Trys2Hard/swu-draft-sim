c9a70a8301883fc61a66b21b3f26110323728164
Keith LePoidevin <klepoidevin3@gmail.com>
2025-07-11 11:56:16 -0700

    Add GA and Icon

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import ReactGA from 'react-ga4';
 import App from './App.jsx'
+
+ReactGA.initialize("G-68WD7D2SY6");
+ReactGA.send({
+  hitType: "pageview",
+  page: window.location.pathname + window.location.search,
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
