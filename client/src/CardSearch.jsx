1abbc3352099cb19dd9dd9742bab4a99429410b1
Keith LePoidevin <klepoidevin3@gmail.com>
2025-09-13 16:25:08 -0700

    Add Basic Styling for Card Search

--- client/src/CardSearch.jsx
+++ client/src/CardSearch.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Box, Typography, Button, Grid } from '@mui/material';
+import { Box, Typography, Button, Grid, FormControl, TextField } from '@mui/material';
 
 export default function CardSearch() {
     const [prompt, setPrompt] = useState("");
@@ -61,112 +61,198 @@ export default function CardSearch() {
     }, [page]);
 
     const styles = {
+        title: {
+            m: '5rem auto 2rem auto',
+            textAlign: 'center',
+            color: 'white',
+        },
+        form: {
+            width: '60%',
+            m: '0 auto 0 auto',
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+        },
+        input: {
+            color: 'white',
+            height: '2.5rem',
+            border: '1px solid white',
+            borderRadius: '25px',
+        },
+        searchButton: {
+            height: '2.5rem',
+            ml: '1rem',
+        },
         cardSearchContainer: {
             width: '90%',
             m: '0 auto 0 auto',
             color: 'white',
         },
-        title: {
-            m: '5rem auto 2rem auto',
-            textAlign: 'center',
-        },
-        form: {
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-        },
     }
 
     return (
-        <Box sx={styles.cardSearchContainer}>
+        <>
             <Typography variant='h4' component='h2' sx={styles.title}>Card Search</Typography>
-            <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <Box sx={styles.formContainer}>
                 <form onSubmit={handleSubmit}>
-                    <input
-                        type="text"
-                        value={prompt}
-                        onChange={(e) => setPrompt(e.target.value)}
-                        placeholder="2 cost green hero..."
-                        style={{ padding: "0.5rem", width: "400px", borderRadius: '5px', border: '1px solid white' }}
-                    />
-                    <Button type="submit" disabled={loading}>
-                        {loading ? "Searching..." : "Search"}
-                    </Button>
+                    <FormControl sx={styles.form}>
+                        <TextField
+                            value={prompt}
+                            fullWidth
+                            onChange={(e) => setPrompt(e.target.value)}
+                            placeholder="2 cost green hero..."
+                            id="card-search"
+                            variant="outlined"
+                            slotProps={{
+                                input: {
+                                    sx: styles.input
+                                },
+                            }} />
+                        <Button variant='contained' sx={styles.searchButton} type="submit">Search</Button>
+                    </FormControl>
                 </form>
             </Box>
-            {cards.length > 0 && (
-                <Box mt={4}>
-                    <Typography variant="h5" gutterBottom>
-                        Found Cards ({total}):
-                    </Typography>
 
-                    {/* Top Pagination Controls */}
-                    <Box display="flex" gap={1} alignItems="center" mb={2}>
-                        <Button
-                            variant="contained"
-                            disabled={loading || page <= 1}
-                            onClick={() => setPage((p) => Math.max(1, p - 1))}
-                        >
-                            Prev
-                        </Button>
-                        <Typography>Page {page} / {totalPages}</Typography>
-                        <Button
-                            variant="contained"
-                            disabled={loading || page >= totalPages}
-                            onClick={() => setPage((p) => Math.min(totalPages, p + 1))}
-                        >
-                            Next
-                        </Button>
-                    </Box>
+            <Box sx={styles.cardSearchContainer}>
+                {cards.length > 0 && (
+                    <Box mt={4}>
+                        <Typography variant="h5" gutterBottom>
+                            Cards Found: {total}
+                        </Typography>
 
-                    {/* Cards Grid */}
-                    <Grid container spacing={2}>
-                        {cards.map((card, index) => (
-                            <Grid key={index}>
-                                <Box display="flex" justifyContent="center" alignItems="center">
-                                    {card.frontArt && (
-                                        <Box
-                                            component="img"
-                                            src={card.frontArt}
-                                            alt={card.name}
+                        {/* Top Pagination Controls */}
+                        <Box sx={{ display: 'flex', gap: 1, alignItems: 'center', mb: 2, }}>
+                            <Button
+                                variant="contained"
+                                disabled={loading || page <= 1}
+                                onClick={() => setPage((p) => Math.max(1, p - 1))}
+                                sx={{
+                                    '&.Mui-disabled': {
+                                        color: 'black',
+                                        backgroundColor: 'rgba(65, 65, 65, 1)',
+                                    }
+                                }}>
+                                Prev
+                            </Button>
+
+                            <Box sx={{ display: 'flex', gap: 2, alignItems: 'center' }}>
+                                {[...Array(totalPages)].map((_, i) => {
+                                    const pageNum = i + 1;
+                                    return (
+                                        <Typography
+                                            key={pageNum}
+                                            onClick={() => setPage(pageNum)}
                                             sx={{
-                                                width: 200,
-                                                height: 'auto',
-                                                borderRadius: 2,
-                                                objectFit: 'cover',
-                                                boxShadow: 3
+                                                cursor: "pointer",
+                                                fontWeight: page === pageNum ? "bold" : "normal",
+                                                textDecoration: page === pageNum ? "underline" : "none",
+                                                "&:hover": { color: "primary.main" },
                                             }}
-                                            onError={(e) => {
-                                                e.target.style.display = 'none';
+                                        >
+                                            {pageNum}
+                                        </Typography>
+                                    );
+                                })}
+                            </Box>
+
+
+                            <Button
+                                variant="contained"
+                                disabled={loading || page >= totalPages}
+                                onClick={() => setPage((p) => Math.min(totalPages, p + 1))}
+                                sx={{
+                                    '&.Mui-disabled': {
+                                        color: 'black',
+                                        backgroundColor: 'rgba(65, 65, 65, 1)',
+                                    }
+                                }}>
+                                Next
+                            </Button>
+                        </Box>
+
+
+                        {/* Cards Grid */}
+                        <Grid container spacing={2}>
+                            {cards.map((card, index) => (
+                                <Grid key={index}>
+                                    <Box display="flex" justifyContent="center" alignItems="center">
+                                        {card.frontArt && (
+                                            <Box
+                                                component="img"
+                                                src={card.frontArt}
+                                                alt={card.name}
+                                                sx={{
+                                                    width: 200,
+                                                    height: 'auto',
+                                                    borderRadius: 2,
+                                                    objectFit: 'cover',
+                                                    boxShadow: 3
+                                                }}
+                                                onError={(e) => {
+                                                    e.target.style.display = 'none';
+                                                }}
+                                            />
+                                        )}
+                                    </Box>
+                                </Grid>
+                            ))}
+                        </Grid>
+
+                        {/* Bottom Pagination Controls */}
+                        <Box sx={{ display: 'flex', gap: 1, alignItems: 'center', mb: 2, }}>
+                            <Button
+                                variant="contained"
+                                disabled={loading || page <= 1}
+                                onClick={() => setPage((p) => Math.max(1, p - 1))}
+                                sx={{
+                                    '&.Mui-disabled': {
+                                        color: 'black',
+                                        backgroundColor: 'rgba(65, 65, 65, 1)',
+                                    }
+                                }}>
+                                Prev
+                            </Button>
+
+                            <Box sx={{ display: 'flex', gap: 2, alignItems: 'center' }}>
+                                {[...Array(totalPages)].map((_, i) => {
+                                    const pageNum = i + 1;
+                                    return (
+                                        <Typography
+                                            key={pageNum}
+                                            onClick={() => setPage(pageNum)}
+                                            sx={{
+                                                cursor: "pointer",
+                                                fontWeight: page === pageNum ? "bold" : "normal",
+                                                textDecoration: page === pageNum ? "underline" : "none",
+                                                "&:hover": { color: "primary.main" },
                                             }}
-                                        />
-                                    )}
-                                </Box>
-                            </Grid>
-                        ))}
-                    </Grid>
+                                        >
+                                            {pageNum}
+                                        </Typography>
+                                    );
+                                })}
+                            </Box>
 
-                    {/* Bottom Pagination Controls */}
-                    <Box display="flex" gap={1} alignItems="center" mt={2}>
-                        <Button
-                            variant="contained"
-                            disabled={loading || page <= 1}
-                            onClick={() => setPage((p) => Math.max(1, p - 1))}
-                        >
-                            Prev
-                        </Button>
-                        <Typography>Page {page} / {totalPages}</Typography>
-                        <Button
-                            variant="contained"
-                            disabled={loading || page >= totalPages}
-                            onClick={() => setPage((p) => Math.min(totalPages, p + 1))}
-                        >
-                            Next
-                        </Button>
-                    </Box>
-                </Box>
-            )}
 
-        </Box>
+                            <Button
+                                variant="contained"
+                                disabled={loading || page >= totalPages}
+                                onClick={() => setPage((p) => Math.min(totalPages, p + 1))}
+                                sx={{
+                                    '&.Mui-disabled': {
+                                        color: 'black',
+                                        backgroundColor: 'rgba(65, 65, 65, 1)',
+                                    }
+                                }}>
+                                Next
+                            </Button>
+                        </Box>
+                    </Box >
+                )
+                }
+
+            </Box >
+        </>
     );
 }
