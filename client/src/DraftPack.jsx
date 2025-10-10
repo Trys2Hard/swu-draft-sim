91d7b43f5b07431e31e4dde3a0c96344908a1566
Keith LePoidevin <klepoidevin3@gmail.com>
2025-10-09 23:36:33 -0700

    DraftPack Responsive Update

--- client/src/DraftPack.jsx
+++ client/src/DraftPack.jsx
@@ -1,37 +1,48 @@
-import { Box, Typography, Grid } from '@mui/material';
+import { Box, Typography, Grid, createTheme, ThemeProvider } from '@mui/material';
 import CardHover from './CardHover';
 import StartButton from './StartButton';
 
 export default function DraftPack({ setName, title, packNum, pickNum, handleStartDraft, draftStarted, draftingLeaders, currentPack, packIndex, handlePopoverClose, handlePopoverOpen, pickCard, anchorEl, hoveredCard, isLoading }) {
+    const theme = createTheme({
+        breakpoints: {
+            values: {
+                xs: 0,
+                sm: 600,
+                md: 725,
+                lg: 900,
+                xl: 960,
+                xxl: 1050,
+            },
+        },
+     });
+
     //Styles
     const styles = {
         packBox: {
-            width: !draftStarted || draftingLeaders ? '95%' : {xs: '95%', sm: '70%'},
+            position: 'relative',
+            width: !draftStarted ? '16rem' : '100%',
             m: '1rem auto 5rem auto',
-            p: '0.5rem',
-            background: 'linear-gradient(to right, rgba(31, 202, 255, 0.2), rgba(31, 202, 255, 0.3), rgba(31, 202, 255, 0.2))',
-            borderRadius: '5px',
+            background: 'rgba(31, 203, 255, 0.15)',
+            borderRadius: !draftStarted ? '5px' : '0px',
             display: 'flex',
             flexDirection: 'column',
             alignItems: 'center',
-            justifyContent: 'center',
+            justifyContent: !draftStarted && 'center',
             color: 'white',
-            minHeight: !draftStarted ? '10rem' : draftingLeaders ? '100%' : '90vh',
+            minHeight: !draftStarted ? '22rem' : draftingLeaders ? '100%' : '110vh',
         },
         pack: {
-            maxWidth: draftingLeaders ? '95%' : {sm: '90%', md: '70%'},
-            height: '100%',
+            width: draftingLeaders ? '100%' : {xs: '95%', sm: '80%', md: '65%', lg:'60%', xl: '55%', xxl: '50%'},
+            maxHeight: '92vh',
             mt: '0.3rem',
             display: 'flex',
             justifyContent: draftingLeaders ? 'center' : 'flex-start',
-            backdropFilter: draftStarted && 'brightness(0.7)',
-            borderRadius: '5px',
-            p: draftStarted && '0.5rem',
+            p: draftStarted && {xs: '0.5rem', md: '1rem'},
             filter: isLoading ? 'blur(2px)' : 'blur(0)',
         },
         card: {
             width: '100%',
-            borderRadius: '10px',
+            borderRadius: draftingLeaders ? '4%' : '4px',
             transition: 'transform 0.3s ease-in-out',
             '&: hover': {
                 cursor: 'pointer',
@@ -52,20 +63,21 @@ export default function DraftPack({ setName, title, packNum, pickNum, handleStar
         <Box sx={styles.packBox}>
             {!draftStarted &&
                 <>
-                    <Typography variant='h4' component='h4' sx={{ position: 'absolute', top: '1rem' }}>{setName}</Typography>
+                    <Typography variant='h5' component='h4' sx={{ position: 'absolute', top: '1rem' }}>{setName}</Typography>
                     <StartButton isLoading={isLoading} onClick={() => handleStartDraft()}>Start Draft</StartButton>
                 </>
             }
             {draftStarted &&
                 <Box>
                     <Box sx={{ display: 'flex', justifyContent: 'center', width: '100%' }}>
-                        <Typography variant='h5' component='h3' sx={{ mr: '1rem' }}>Pack: {packNum}</Typography>
-                        <Typography variant='h5' component='h3' sx={{ ml: '1rem' }}>Pick: {pickNum}</Typography>
+                        <Typography variant='h5' component='h3' sx={{ mr: '1rem', mt: '3rem' }}>Pack: {packNum}</Typography>
+                        <Typography variant='h5' component='h3' sx={{ ml: '1rem', mt: '3rem' }}>Pick: {pickNum}</Typography>
                     </Box>
                 </Box>
             }
             {draftStarted &&
-                    <Grid container spacing={draftingLeaders ? 3 : {xs: 0.3, sm: 1}} sx={styles.pack}>
+                <ThemeProvider theme={theme}>
+                    <Grid container spacing={draftingLeaders ? 3 : 1} sx={styles.pack}>
                         {currentPack[packIndex]?.map((card) => {
                             const cardId = `card-id-${card.id}`;
                             return (
@@ -93,6 +105,7 @@ export default function DraftPack({ setName, title, packNum, pickNum, handleStar
                             onHoverClose={handlePopoverClose} />
                             <Typography variant='h3' component='p' sx={styles.loading}>Loading...</Typography>
                     </Grid>
+                </ThemeProvider>
             }
         </Box>
     );
