37b27f62a33a62a04ef4f94bb53dea14102e2c0e
Keith LePoidevin <94937403+Trys2Hard@users.noreply.github.com>
2026-01-05 12:16:34 -0800

    Rotate Hover Image for Bases

--- client/src/Components/CardHover.jsx
+++ client/src/Components/CardHover.jsx
@@ -3,15 +3,22 @@ import { Box, Popover } from '@mui/material';
 export default function CardHover({ anchorEl, hoveredCard, onHoverClose }) {
   const open = Boolean(anchorEl);
   const hoveredCardData = hoveredCard?.cardData;
+  const isBase = hoveredCardData?.Type === 'Base';
 
   //Styles
   const styles = {
+    containerBox: {
+      display: 'flex',
+      alignItems: 'center',
+      minWidth: isBase ? '28rem' : 'auto',
+    },
     frontArtPopover: {
       width: '20rem',
       height: 'auto',
       aspectRatio: hoveredCardData?.Type === 'Leader' ? '7/5' : '5/7',
       borderRadius: '15px',
-      ml: '6px',
+      ml: isBase ? '4.4rem' : '6px',
+      transform: isBase ? 'rotate(90deg)' : 'none',
     },
     backArtPopover: {
       width: '20rem',
@@ -19,6 +26,7 @@ export default function CardHover({ anchorEl, hoveredCard, onHoverClose }) {
       aspectRatio: '5/7',
       borderRadius: '15px',
       ml: '3px',
+      transform: isBase ? 'rotate(90deg)' : 'none',
     },
   };
 
@@ -30,6 +38,7 @@ export default function CardHover({ anchorEl, hoveredCard, onHoverClose }) {
         '& .MuiPaper-root': {
           backgroundColor: 'transparent',
           boxShadow: 'none',
+          overflow: 'visible',
         },
       }}
       open={open}
@@ -47,7 +56,7 @@ export default function CardHover({ anchorEl, hoveredCard, onHoverClose }) {
       disableScrollLock
     >
       {hoveredCard && (
-        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+        <Box sx={styles.containerBox}>
           <Box
             component="img"
             src={hoveredCardData?.FrontArt}
